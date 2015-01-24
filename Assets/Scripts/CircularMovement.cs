1c6a23a3706faa22f32748ecd1a010e8472ef65c
akshay91r <akshay91r@gmail.com>
2015-01-24 01:28:09 -0500

    first commit

---
+++ Assets/Scripts/CircularMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircularMovement : MonoBehaviour  {
+	
+	private Vector3 center = new Vector3(0,0,0);
+	public float degreesPerSecond = -65.0f;
+	
+	private Vector3 v;
+	
+	void Start() {
+		v = transform.position - center;
+	}
+	
+	//values for internal use
+	private Quaternion _lookRotation;
+	private Vector3 _direction;
+	
+	void Update () {
+		v = Quaternion.AngleAxis (degreesPerSecond * Time.deltaTime, Vector3.forward) * v;
+		transform.position = center + v;
+
+		transform.RotateAround(center, Vector3.forward, degreesPerSecond * Time.deltaTime);
+
+	}
+}
